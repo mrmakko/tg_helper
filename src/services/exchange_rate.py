b6e055efa3351105ce648c1447d4650c9cdb484f
Makko <mrmrmakko@gmail.com>
2025-02-21 12:09:39 +0700

    Исправлено округление

--- src/services/exchange_rate.py
+++ src/services/exchange_rate.py
@@ -5,11 +5,11 @@ def get_baht_to_ruble() -> str:
     data = response.json()
     rate = data["rates"]["RUB"]
     adjusted_rate = round(rate * 1.10 / 0.05) * 0.05
-    return f'{adjusted_rate} (исходный курс: {rate})'
+    return f'{adjusted_rate:.2f} (исходный курс: {rate})'
 
 def get_usdt_to_baht() -> str:
     response = requests.get("https://api.exchangerate-api.com/v4/latest/USDT")
     data = response.json()
     rate = data["rates"]["THB"]
     adjusted_rate = round(rate * 0.975 / 0.05) * 0.05
-    return f'{adjusted_rate} (исходный курс: {rate})'
+    return f'{adjusted_rate:.2f} (исходный курс: {rate})'
